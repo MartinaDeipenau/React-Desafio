eca5777b7b0292618418783490772f0b03962f1e
martina deipenau <martinadeipenau02@gmail.com>
2022-11-13 03:10:56 -0300

    add: updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
-    <NavBar/>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<ItemListContainer />} />
+        <Route path='/category/:idCategory'element={<ItemListContainer />} />
+        <Route path='/detail'element={<ItemDetailContainer />} />
+        <Route path='/vacio'element={<h1>Vacio</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App; 
